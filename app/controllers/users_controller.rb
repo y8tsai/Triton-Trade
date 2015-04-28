c8239ba77619926e1c73013527503be5deacb155
Michael Park <mjp014@ucsd.edu>
2015-04-28 14:13:22 -0700

    reverting users_controller.rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,10 @@
 # change this later
-class UsersController < ActionController::Base
-  def create
-    User.create(user_params)
-  end
-  private:
-  def user_params
-    params.require(:user).permit(:name, :provider, :uid, :email);
-  end
-end
+#class UsersController < ActionController::Base
+#  def create
+#    User.create(user_params)
+#  end
+#  private:
+#  def user_params
+#    params.require(:user).permit(:name, :provider, :uid, :email);
+#  end
+#end
