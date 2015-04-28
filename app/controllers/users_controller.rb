32a97af01987f5d8bd5f0c1673834beef3d5d906
Michael Park <mjp014@ucsd.edu>
2015-04-27 21:34:25 -0700

    Testing embedded ruby on homepage

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,10 @@
+# change this later
+class UsersController < ActionController::Base
+  def create
+    User.create(user_params)
+  end
+  private:
+  def user_params
+    params.require(:user).permit(:name, :provider, :uid, :email);
+  end
+end
