8156158619592ac4bad8aeaa26c01b591c096ef9
Steven Jin <stjin@ucsd.edu>
2015-04-30 15:39:22 -0700

    Routing fix?

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -10,4 +10,18 @@ def facebook
       redirect_to new_user_registration_url
     end
   end
+
+  # Simple Google Login implementation
+  def google_oauth2
+    @user = User.find_for_google_oauth2(request.env["omniauth.auth"], current_user)
+
+    if @user.persisted?
+      flash[:notice] = I18n.t "devise.omniauth_callbacks.success", :kind => "Google"
+      sign_in_and_redirect @user, :event => :authentication
+    else
+      session["devise.google_data"] = request.env["omniauth.auth"]
+      redirect_to new_user_registration_url
+    end
+  end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -29,19 +29,6 @@ class User < ActiveRecord::Base
     end
   end
 
-  # Simple Google Login implementation
-  def google_oauth2
-    @user = User.find_for_google_oauth2(request.env["omniauth.auth"], current_user)
-
-    if @user.persisted?
-      flash[:notice] = I18n.t "devise.omniauth_callbacks.success", :kind => "Google"
-      sign_in_and_redirect @user, :event => :authentication
-    else
-      session["devise.google_data"] = request.env["omniauth.auth"]
-      redirect_to new_user_registration_url
-    end
-  end
-
   # Bind/Create User
   def self.find_for_google_oauth2(access_token, signed_in_resource=nil)
     data = access_token.info
