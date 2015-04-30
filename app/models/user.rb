59f68277a80806e40397c84f1539ea0b7c2209c3
Steven Jin <stjin@ucsd.edu>
2015-04-30 15:05:13 -0700

    Y u do this heroku

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable,
-         :omniauthable, :omniauth_providers => [:google_oauth2]
+         :omniauthable, :omniauth_providers => [:google_oauth2, :facebook]
 
   def user_params
     params.require(:user).permit(:username, :email, :password, :password_confirmation)
