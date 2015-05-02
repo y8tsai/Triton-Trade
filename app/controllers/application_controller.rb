14c81fb50f56ca48766b737de4d0135c87aba78c
Yi-Chen Tsai <y8tsai@ucsd.edu>
2015-05-02 16:35:28 -0700

    Still fixing profiles..

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,6 +9,6 @@ class ApplicationController < ActionController::Base
 
         def configure_permitted_parameters
             devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:name, :email, :password) }
-            devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:name, :email, :password, :current_password, :PID, :is_female, :date_of_birth) }
+            devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:name, :email, :password, :current_password, :PID, :date_of_birth, :is_female) }
         end
 end
