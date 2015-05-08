9928df9ecdd6dd2d4a8628094bc366723cc10a96
Yi-Chen Tsai <y8tsai@ucsd.edu>
2015-05-08 15:12:11 -0700

    testing

--- db/migrate/20150428221735_add_username_to_users.rb
+++ db/migrate/20150428221735_add_username_to_users.rb
@@ -2,9 +2,10 @@ class AddUsernameToUsers < ActiveRecord::Migration
   def change
     add_column :users, :username, :string
     add_index :users, :username, unique: true
-    #Added for Profile Info
+  
     add_column :users, :PID, :string
     add_column :users, :date_of_birth, :datetime
     add_column :users, :is_female, :boolean, default:false
+    add_column :users, :avatar_url, :string
   end
 end
