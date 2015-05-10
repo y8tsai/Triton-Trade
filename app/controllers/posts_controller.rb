21f49f247c112c6ba2d4beffbd4fd71e577e401c
Michael Park <mjp014@ucsd.edu>
2015-05-09 19:43:56 -0700

    Added redirection to login page

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-  before_action :set_post, only: [:show, :edit, :update, :destroy]
+  before_action :set_post, :authenticate_user!, only: [:show, :edit, :update, :destroy]
 
   respond_to :html
 
