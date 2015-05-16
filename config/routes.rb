5a6e5039da4e8b388cf00e088cd7a939b352b506
Yi-Chen Tsai <y8tsai@ucsd.edu>
2015-05-15 19:11:31 -0700

    adding views for items

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :posts
+  resources :items
 
   devise_for :users, :controllers => { :omniauth_callbacks => "omniauth_callbacks" }
   resources :widgets
