52103c51d53db43d107caf2514ea692eef743c6b
Yi-Chen Tsai <y8tsai@ucsd.edu>
2015-05-15 20:21:20 -0700

    Edited items controller

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,18 +1,47 @@
 class ItemsController < ApplicationController
-    def index
-      @items = Item.all
-    end
+before_action :set_item, :authenticate_user!, only: [:show, :edit, :update, :destroy]
 
-    def show
+  respond_to :html
+
+  def index
+    @items = item.all
+    respond_with(@items)
+  end
+
+  def show
+    respond_with(@item)
+  end
+
+  def new
+    @item = item.new
+    respond_with(@item)
+  end
+
+  def edit
+  end
+
+  def create
+    @item = item.new(item_params)
+    @item.save
+    respond_with(@item)
+  end
+
+  def update
+    @item.update(item_params)
+    respond_with(@item)
+  end
+
+  def destroy
+    @item.destroy
+    respond_with(@item)
+  end
+
+  private
+    def set_item
       @item = Item.find(params[:id])
     end
 
-    def create
-      Item.create(item_params)
-    end
-    
-    private
     def item_params
-      params.require(:user).permit(:name, :description, :is_sold, :price, :condition, :image_url);
+      params.require(:item).permit(:name, :description, :is_sold, :price, :condition, :image_url, :user_id);
     end
 end
