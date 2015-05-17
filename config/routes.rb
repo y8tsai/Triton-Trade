d81beeade3bbabb36442c5cd1469bdc0d139e4a5
Yi-Chen Tsai <y8tsai@ucsd.edu>
2015-05-16 18:53:50 -0700

    Adding tags to items

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,7 @@ Rails.application.routes.draw do
   root 'pages#index'
   get 'testpage' => 'pages#testpage'  
   get 'info' => 'pages#info'
+  get 'tags/:tag', to: 'items#index', as: "tag"
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
