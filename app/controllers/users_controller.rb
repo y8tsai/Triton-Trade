7a97aac6e2b3a606c40084a8f3da862b88cf63f9
Yi-Chen Tsai <y8tsai@ucsd.edu>
2015-05-26 23:30:42 -0700

    Shows items purchased by the user on the user profile page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,7 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @items = Item.where(user_id: params[:id])
-    @purchaseditems = Item.where(buyeremail: params[:id])
+    @purchaseditems = Item.where(buyeremail: params[:email])
   end
 
   def create
