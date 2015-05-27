687c2d1a93e56a5d0f9a1cca4252734e0e5402d7
Yi-Chen Tsai <y8tsai@ucsd.edu>
2015-05-26 23:54:38 -0700

    More testing

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,7 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @items = Item.where(user_id: params[:id])
-    @purchaseditems = Item.where(user_id: params[:id])
+    @purchaseditems = Item.where(buyeremail: User.find(params[:id]).email )
   end
 
   def create
