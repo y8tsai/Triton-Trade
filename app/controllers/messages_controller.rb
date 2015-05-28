e650223d61a701dc421e41e9f02ba5147ae3e5b5
Michael Park <mjp014@ucsd.edu>
2015-05-27 22:58:58 -0700

    Updating message controller

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,12 +1,42 @@
 class MessagesController < ApplicationController
+  respond_to :html
+
   def index
     @messages = current_user.received_messages unless current_user.nil?
+    respond_with(@messages)
   end
 
   def show
     @message = current_user.received_messages.find(params[:id]) unless current_user.nil?
+    respond_with(@messages)
   end
 
   def new
+    @message = Message.new
+    respond_with(@message)
   end
+
+  def edit
+  end
+
+  def create
+    @message = Message.new(message_params)
+    @message.save
+    respond_with(@message)
+  end
+
+  def update
+    @message.update(message_params)
+    respond_with(@message)
+  end
+
+  def destroy
+    @message.destroy
+    respond_with(@message)
+  end
+
+  private
+    def message_params
+      params.require(:message).permit(:user_id, :recipient_id, :subject, :body)
+    end
 end
--- db/migrate/20150527055403_add_purchased_by_to_items.rb~
+++
@@ -1,5 +0,0 @@
-class AddPurchasedByToItems < ActiveRecord::Migration
-  def change
-      add_column :items, :buyeremail, :string, null: false, default: "" 
-  end
-end
