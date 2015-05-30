92a5590316eec1fb97f9963b7068d03e63973cb8
Yi-Chen Tsai <y8tsai@ucsd.edu>
2015-05-30 16:41:30 -0700

    try search on homepage

--- app/models/search.rb
+++ app/models/search.rb
@@ -10,7 +10,7 @@ def find_items
     items =Item.order(:name)
     items =items.where("name ilike ?", "%#{name}%") if name.present?
     items =items.where("category ilike ?", "%#{category}%") if category.present?
-    items =items.where("tag ilike ?", "%#{tag}%") if tag.present?
+    #items =items.where("tag ilike ?", "%#{tag}%") if tag.present?
     items =items.where("condition ilike ?", "%#{condition}%") if condition.present?
     items =items.where("price >= ?", minimum_price) if minimum_price.present?
     items =items.where("price <= ?", maximum_price) if maximum_price.present?
