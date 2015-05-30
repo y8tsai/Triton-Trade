1e6ee719e39695c11857cbc55a2faf6a21948b75
Yi-Chen Tsai <y8tsai@ucsd.edu>
2015-05-30 15:16:26 -0700

    Mystic powers grant me a miracle!

--- app/models/search.rb
+++ app/models/search.rb
@@ -7,12 +7,13 @@ end
 private
 
 def find_items
-    items =Item.where("name like ?", "%#{name}#%") if name.present?
-    items =Item.where("category like ?", "%#{category}#%") if category.present?
+    items =Item.order(:name)
+    items =items.where("name like ?", "%#{name}#%") if name.present?
+    items =items.where("category like ?", "%#{category}#%") if category.present?
     #items =items.where("tag like ?", "%#{tag}#%") if tag.present?
-    items =Item.where("condition like ?", "%#{condition}#%") if condition.present?
-    items =Item.where("price >= ?", minimum_price) if minimum_price.present?
-    items =Item.where("price <= ?", maximum_price) if maximum_price.present?
+    items =items.where("condition like ?", "%#{condition}#%") if condition.present?
+    items =items.where("price >= ?", minimum_price) if minimum_price.present?
+    items =items.where("price <= ?", maximum_price) if maximum_price.present?
     items
 end
 
