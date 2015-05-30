c73de53e89afa65d6e2721fac0b96e2112f247d9
Yi-Chen Tsai <y8tsai@ucsd.edu>
2015-05-30 12:43:20 -0700

    More search stuff

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,20 +1,30 @@
 class SearchesController < ApplicationController
 
+respond_to :html
+
 def index
 	@searches = Search.all
+	respond_with(@searches)
 end
 
 def show
-	@search = Search.new(search_params)
+	respond_with(@search)
 end
 
 def new
 	@search = Search.new
+	respond_with(@search)
 end
 
 def create
 	@search = Search.new(search_params)
 	@search.save
+	respond_with(@search)
+end
+
+def destroy
+	@search = Search.destroy
+	respond_with(@search)
 end
 
 private 
