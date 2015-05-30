827e6ee0f16097819423b8e052e7122f15644778
Yi-Chen Tsai <y8tsai@ucsd.edu>
2015-05-30 16:07:58 -0700

    OMG, search works

--- app/models/search.rb
+++ app/models/search.rb
@@ -9,11 +9,11 @@ private
 def find_items
     items =Item.order(:name)
     items =items.where("name like ?", "%#{name}%") if name.present?
-    #items =items.where("category like ?", "%#{category}#%") if category.present?
-    #items =items.where("tag like ?", "%#{tag}#%") if tag.present?
-    #items =items.where("condition like ?", "%#{condition}#%") if condition.present?
-    #items =items.where("price >= ?", minimum_price) if minimum_price.present?
-    #items =items.where("price <= ?", maximum_price) if maximum_price.present?
+    items =items.where("category like ?", "%#{category}%") if category.present?
+    items =items.where("tag like ?", "%#{tag}%") if tag.present?
+    items =items.where("condition like ?", "%#{condition}%") if condition.present?
+    items =items.where("price >= ?", minimum_price) if minimum_price.present?
+    items =items.where("price <= ?", maximum_price) if maximum_price.present?
     items
 end
 
