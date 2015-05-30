a790e1ff630eaa7f65b90aa8e83cacd541c1aa87
Yi-Chen Tsai <y8tsai@ucsd.edu>
2015-05-30 15:34:47 -0700

    8177616

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -13,13 +13,13 @@ def new
 end
 
 def create
-	@search = Search.create!(params[:search])
+	@search = Search.create(search_params)
 	redirect_to @search
 end
 
-#private 
-#  def search_params
-#	params.require(:search).permit(:name, :user_email, :tag, :condition, :minimum_price, :maximum_price, :category);
-#  end
+private 
+  def search_params
+	params.require(:search).permit(:name, :user_email, :tag, :condition, :minimum_price, :maximum_price, :category);
+  end
 
 end
