a0820ff6e81501b7e691007fb8f079fd3a7a553f
Yi-Chen Tsai <y8tsai@ucsd.edu>
2015-05-30 16:12:04 -0700

    Making name search case insensitive

--- app/models/search.rb
+++ app/models/search.rb
@@ -8,10 +8,10 @@ private
 
 def find_items
     items =Item.order(:name)
-    items =items.where("name like ?", "%#{name}%") if name.present?
-    items =items.where("category like ?", "%#{category}%") if category.present?
-    items =items.where("tag like ?", "%#{tag}%") if tag.present?
-    items =items.where("condition like ?", "%#{condition}%") if condition.present?
+    items =items.where("name ilike ?", "%#{name}%") if name.present?
+    items =items.where("category ilike ?", "%#{category}%") if category.present?
+    items =items.where("tag ilike ?", "%#{tag}%") if tag.present?
+    items =items.where("condition ilike ?", "%#{condition}%") if condition.present?
     items =items.where("price >= ?", minimum_price) if minimum_price.present?
     items =items.where("price <= ?", maximum_price) if maximum_price.present?
     items
